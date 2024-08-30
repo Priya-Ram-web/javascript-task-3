f83da2bc399a03f38ac310d167e2338e8c99e7d2
Priya-Ram-web <basudey456@gmail.com>
2024-08-30 22:06:38 +0600

    checking-arrays is done

---
+++ checking-arrays.js
@@ -0,0 +1,8 @@
+const book =['Chemistry','Physics','Biology'];
+console.log(Array.isArray(book));
+const books = ['high er ma th'];
+console.log(Array.isArray(books));
+const storyBook =[];
+console.log(Array.isArray(storyBook));
+const gostBook = 5;
+console.log(Array.isArray(gostBook));
