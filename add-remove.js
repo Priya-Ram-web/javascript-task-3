9a6172911e181f61f4d5566c687b61530d1494b1
Priya-Ram-web <basudey456@gmail.com>
2024-08-30 20:55:05 +0600

    add-remove task is end

---
+++ add-remove.js
@@ -0,0 +1,7 @@
+const tourist =['Ranghamati','Bandhorban','Coxs-Bazar'];
+console.log(tourist);
+tourist.push('Pottengha');
+tourist.push('Sent-Martin');
+console.log(tourist);
+tourist.pop();
+console.log(tourist);
